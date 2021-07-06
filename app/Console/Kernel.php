f09a91762bf2ccaa12096df894bb6de7afa96245
bob <elmertorres523@gmail.com>
2021-07-06 16:07:04 +0800

    change timezone

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,8 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('SendSms:Message')->cron('0 8 * * *');
-        
-        $schedule->command('SendSms:Message')->cron('0 16 * * *');
+        $schedule->command('SendSms:Message')->cron('10 16 * * *');
     }
 
     /**
