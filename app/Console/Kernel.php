4fd63d4bf35614269da458badd720418c3da3e70
bob <elmertorres523@gmail.com>
2021-07-06 15:16:51 +0800

    add cron sms daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('SendSms:Message')->cron('0 8 * * *');
+        // $schedule->command('SendSms:Message')->cron('0 8 * * *');
+        $schedule->command('SendSms:Message')->cron('30 15 * * *');
     }
 
     /**
