042b7af562684b606968ff6d3ca1bbc5bc011e9f
bob <elmertorres523@gmail.com>
2021-08-08 22:55:52 +0800

    add average slp

--- app/Http/Controllers/Api/AxieController.php
+++ app/Http/Controllers/Api/AxieController.php
@@ -26,6 +26,11 @@ class AxieController extends Controller
             $details              = self::index($item->roninAddress);
             $updated_on           = Carbon::createFromTimestamp($details['updated_on'])->format('F d, Y h:i:s a');
             $last_claim_timestamp = Carbon::createFromTimestamp($details['last_claim_timestamp'])->format('F d, Y h:i:s a');
+            $next_claim_timestamp = Carbon::createFromTimestamp($details['last_claim_timestamp'])->addDays(14)->format('F d, Y h:i:s a');;
+
+            $now                  = Carbon::now();
+            $last_claim_datetime  = Carbon::parse(Carbon::createFromTimestamp($details['last_claim_timestamp'])->format('Y-m-d'));
+            $last_claim_date_diff = $last_claim_datetime->diffInDays($now);
 
             $data[] = [
                 'roninAddress'       => $item->roninAddress,
@@ -33,10 +38,12 @@ class AxieController extends Controller
                 'managerShare'       => $item->managerShare,
                 'scholarShare'       => 100 - $item->managerShare,
                 'ign'                => $details['ign'],
+                'lastClaimDateDiff'  => $last_claim_date_diff,
                 'totalSlp'           => $details['total_slp'],
                 'inGameSlp'          => $details['in_game_slp'],
                 'updatedOn'          => $updated_on,
                 'lastClaimTimestamp' => $last_claim_timestamp,
+                'nextClaimTimestamp' => $next_claim_timestamp,
                 'lastClaimAmount'    => $details['last_claim_amount'],
                 'gameStatsSuccess'   => $details['game_stats_success'],
                 'winRate'            => $details['win_rate'],
